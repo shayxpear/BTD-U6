08c5e3c526faa550dcbb1ba45d26a8ab6ab33126
shayxpear <shayxpear@gmail.com>
2025-05-08 14:57:32 -0700

    implemented multiple track system

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -4,12 +4,22 @@ using UnityEngine.SceneManagement;
 public class DoorManager : MonoBehaviour
 {
     public string nextScene;
+    public NoteManager noteManager;
+    public GameObject noteManagerGameObject;
+
+    public void Start()
+    {
+        noteManagerGameObject = GameObject.Find("NoteManager");
+        noteManager = noteManagerGameObject.GetComponent<NoteManager>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            noteManager.levelsBeaten++;
             SceneManager.LoadScene(nextScene);
+            
         }
     }
 }
---
+++ Assets/Scripts/TrackHolder.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class TrackHolder : MonoBehaviour
+{
+    public string midiPath;
+    public AudioClip[] guitarRiffClip;
+    public AudioSource guitarRiff;
+
+    private NoteManager noteManager;
+    private GameObject noteManagerGameObject;
+
+    public void Start()
+    {
+        noteManagerGameObject = GameObject.Find("NoteManager");
+        noteManager = noteManagerGameObject.GetComponent<NoteManager>();
+    }
+
+    void Update()
+    {
+        if (noteManager.levelsBeaten == guitarRiffClip.Length)
+        {
+            noteManager.levelsBeaten = 0;
+        }
+
+        switch (guitarRiff.clip.name)
+        {
+            case "guitar riff":
+                midiPath = "test1.mid";
+                guitarRiff.clip = guitarRiffClip[0];
+                Debug.Log("Successful switch case");
+                break;
+            case "battle song finished":
+                midiPath = "test.mid";
+                guitarRiff.clip = guitarRiffClip[1];
+                Debug.Log("Successful switch case");
+                break;
+        }
+
+        guitarRiff.clip = guitarRiffClip[noteManager.levelsBeaten];
+
+        
+
+    }
+}
