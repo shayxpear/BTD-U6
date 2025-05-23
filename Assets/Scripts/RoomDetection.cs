174111eb18c020835d08b1cee1f1a0cc1f7cc749
shayxpear <shayxpear@gmail.com>
2025-05-23 00:32:55 -0700

    Fixed song transitions

--- Assets/Scripts/RoomDetection.cs
+++ Assets/Scripts/RoomDetection.cs
@@ -5,13 +5,14 @@ public class RoomDetection : MonoBehaviour
     public bool playerInRange;
     public GameObject Doors;
     public BetterNoteManager noteManager;
-    public GameObject noteManagerGameObject;
+    public TrackHolder trackHolder;
 
     private int enemiesInRange = 0;
     public void Start()
     {
-        noteManagerGameObject = GameObject.Find("NoteManager");
-        noteManager = noteManagerGameObject.GetComponent<BetterNoteManager>();
+
+        noteManager = GameObject.Find("NoteManager").GetComponent<BetterNoteManager>();
+        trackHolder = GameObject.Find("Track Holder").GetComponent<TrackHolder>();
         noteManager.started = false;
         noteManager.startedRiff = false;
     }
@@ -33,10 +34,12 @@ public class RoomDetection : MonoBehaviour
             if (!noteManager.started)
             {
                 noteManager.StartSong();
+                trackHolder.backgroundSong.Stop();
             }
             else
             {
                 noteManager.StartSong();
+                trackHolder.backgroundSong.Stop();
             }
 
             noteManager.ended = false;
@@ -45,6 +48,7 @@ public class RoomDetection : MonoBehaviour
         {
             Doors.SetActive(false);
             noteManager.ended = true;
+            if (!trackHolder.backgroundSong.isPlaying) { trackHolder.backgroundSong.Play(); }
         }
     }
 
